6647511555307f29490c4b286f7e6789aa84fdf9
Fil Maj <filip.maj@nitobi.com>
2009-07-18 16:38:29 -0700

    Added Beer Me as a Phonegap application, woo! Shamelessly incorporated jonleblancs JavaScript-YQL bridge utilities (find it here: http://github.com/jonleblanc/yql-utilities/tree/master).

---
+++ src/com/phonegap/demo/GeoListener.java
@@ -0,0 +1,69 @@
+package com.phonegap.demo;
+
+import android.content.Context;
+import android.location.Location;
+import android.webkit.WebView;
+
+public class GeoListener {
+	String id;
+	String successCallback;
+	String failCallback;
+    GpsListener mGps; 
+    NetworkListener mNetwork;
+    Context mCtx;
+    private WebView mAppView;
+	
+	int interval;
+	
+	GeoListener(String i, Context ctx, int time, WebView appView)
+	{
+		id = i;
+		interval = time;
+		mCtx = ctx;
+        mGps = new GpsListener(mCtx, interval, this);
+        mNetwork = new NetworkListener(mCtx, interval, this);
+        mAppView = appView;
+	}
+	
+	void success(Location loc)
+	{
+		/*
+		 * We only need to figure out what we do when we succeed!
+		 */
+		if(id != "global")
+		{
+			mAppView.loadUrl("javascript:Geolocation.success(" + id + ", " + loc.getLatitude() + ", " + loc.getLongitude() + ")");
+		}
+		else
+		{
+			mAppView.loadUrl("javascript:Geolocation.gotCurrentPosition(" + loc.getLatitude() + ", " + loc.getLongitude() + ")");
+			this.stop();
+		}
+	}
+	
+	void fail()
+	{
+		// Do we need to know why?  How would we handle this?
+		if (id != "global") {
+			mAppView.loadUrl("javascript:Geolocation.fail(" + id + ")");
+		}
+		else
+		{
+			mAppView.loadUrl("javascript:Geolocation.fail()");
+		}
+	}
+	
+	// This stops the listener
+	void stop()
+	{
+		mGps.stop();
+		mNetwork.stop();
+	}
+
+	public Location getCurrentLocation() {
+		Location loc = mGps.getLocation();
+		if (loc == null)
+			loc = mNetwork.getLocation();
+		return loc;
+	}
+}
