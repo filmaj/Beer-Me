4045c660b100c1e491475e6099dc0909e6f83bd3
Fil Maj <filip.maj@nitobi.com>
2009-07-27 22:54:43 -0700

    XML parser class.

---
+++ src/com/nitobi/LocationXMLParser.java
@@ -0,0 +1,201 @@
+package com.nitobi;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * A web service geo-location XML parser. Able to parse a (result) -> (location) type tree XML data. Is constructed by providing a service URL and XML tag names to parse for.
+ * Parses into Place objects.
+ * @author Fil Maj
+ *
+ */
+public class LocationXMLParser extends DefaultHandler
+{     
+	private ArrayList<Place> places;
+    private String urlString;
+    private String currentPlace;
+    private String nameElementTag;
+    private String currentAddress;
+    private String addressElementTag;
+    private String currentCity;
+    private String cityElementTag;
+    private String currentState;
+    private String stateElementTag;
+    private String currentPhone;
+    private String phoneElementTag;
+    private String currentLink;
+    private String urlElementTag;
+    private double currentLat;
+    private String latElementTag;
+    private double currentLon;
+    private String lonElementTag;
+    private boolean inResult;
+    private String resultElementTag;
+    private StringBuilder text;
+    private boolean parseBeerMapping = false;
+    
+    public LocationXMLParser(String nameEl, String addressEl, String cityEl, String stateEl, String phoneEl, String urlEl, String latEl, String lonEl, String resultEl) {
+        this.nameElementTag = nameEl;
+        this.addressElementTag = addressEl;
+        this.cityElementTag = cityEl;
+        this.stateElementTag = stateEl;
+        this.phoneElementTag = phoneEl;
+        this.urlElementTag = urlEl;
+        this.latElementTag = latEl;
+        this.lonElementTag = lonEl;
+        this.resultElementTag = resultEl;
+        this.text = new StringBuilder();
+        this.places = new ArrayList<Place>();
+        this.currentLat = 0;
+        this.currentLon = 0;
+        this.currentPlace = null;
+    }
+    public void shouldParseBeerMapping(boolean should) {
+    	this.parseBeerMapping = should;
+    }
+    public void setRequestURL(String url) {
+    	this.urlString = url;
+    }
+    public void parse() throws ParserConfigurationException, SAXException, IOException {
+    	if (this.urlString != null && this.urlString.length() > 0) {
+			InputStream urlInputStream = null;
+			SAXParserFactory spf = null;
+			SAXParser sp = null;
+			URL url = new URL(this.urlString);
+			urlInputStream = url.openConnection().getInputStream();
+			spf = SAXParserFactory.newInstance();
+			if (spf != null) {
+				sp = spf.newSAXParser();
+				sp.parse(urlInputStream, this);
+			}
+			if (urlInputStream != null)
+				urlInputStream.close();
+		}
+    }
+    /**
+     * Logic executed when the parser encounters a new XML element.
+     */
+    public void startElement(String uri, String localName, String qName, Attributes attributes) {
+    	// If it's a new 'result' element, reset the local variables.
+        if (localName.equalsIgnoreCase(this.resultElementTag)) {
+                this.inResult = true;
+                this.currentLat = 0;
+                this.currentLon = 0;
+                this.currentPlace = null;
+                this.currentAddress = null;
+                this.currentCity = null;
+                this.currentState = null;
+                this.currentPhone = null;
+                this.currentLink = null;
+        }
+    }
+   
+    /**
+     * Logic executed when the parser encounters the end of an XML element. This is where we actually parse for the elements contents.
+     */
+    public void endElement(String uri, String localName, String qName) {
+        // Check we have a Result
+        if (this.inResult == false) {
+            return;
+        }
+        // Check are at the end of a result item. Here is where we construct a Place instance and add it to our list.
+        if (localName.equalsIgnoreCase(this.resultElementTag)) {
+                this.inResult = false;
+                // Make sure we have, at the least, a place name.
+                if (this.currentPlace != null && this.currentPlace.length() > 0) {
+                	Place obj = new Place(this.currentPlace);
+                	obj.lat = this.currentLat;
+                	obj.lng = this.currentLon;
+                	// Construct an address string based on available address, city and state information.
+                	String finalAddressString = "";
+                	if (this.currentState != null) {
+                		finalAddressString = this.currentState;
+                	}
+                	if (this.currentCity != null) {
+                		finalAddressString = this.currentCity + ", " + finalAddressString;
+                	}
+                	if (this.currentAddress != null)  {
+                		finalAddressString = this.currentAddress + "\n" + finalAddressString;
+                	}
+                	obj.address = finalAddressString;
+                	if (this.currentPhone != null) {
+                		obj.phone = this.currentPhone;
+                	}
+                	if (this.currentLink != null) {
+                		obj.reviewlink = this.currentLink;
+                	}
+                	if (this.parseBeerMapping)
+                		obj.isBeerMapping = true;
+                	this.places.add(obj);
+                }
+        }
+        // Parse title
+        if (localName.equalsIgnoreCase(this.nameElementTag))
+        {
+            if (this.inResult == true){
+                this.currentPlace = this.text.toString().trim();
+            } 
+        }       
+        // Parse geo coords
+        if (localName.equalsIgnoreCase(this.latElementTag))
+        {
+            if (this.inResult == true){
+                this.currentLat = Double.parseDouble(this.text.toString().trim());
+            } 
+        }   
+        if (localName.equalsIgnoreCase(this.lonElementTag))
+        {
+            if (this.inResult == true){
+                this.currentLon = Double.parseDouble(this.text.toString().trim());
+            } 
+        }
+        if (localName.equalsIgnoreCase(this.addressElementTag))
+        {
+        	if (this.inResult == true){
+                this.currentAddress = this.text.toString().trim();
+            }
+        }
+        if (localName.equalsIgnoreCase(this.cityElementTag))
+        {
+        	if (this.inResult == true){
+                this.currentCity = this.text.toString().trim();
+            }
+        }
+        if (localName.equalsIgnoreCase(this.stateElementTag))
+        {
+        	if (this.inResult == true){
+                this.currentState = this.text.toString().trim();
+            }
+        }
+        if (localName.equalsIgnoreCase(this.phoneElementTag))
+        {
+        	if (this.inResult == true){
+                this.currentPhone = this.text.toString().trim();
+            }
+        }
+        if (localName.equalsIgnoreCase(this.urlElementTag))
+        {
+        	if (this.inResult == true){
+                this.currentLink = this.text.toString().trim();
+            }
+        }
+        this.text.setLength(0);
+    }
+   
+    public void characters(char[] ch, int start, int length) {
+        this.text.append(ch, start, length);
+    }
+    public ArrayList<Place> getPlaces() {
+    	return this.places;
+    }
+}
