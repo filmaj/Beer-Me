846723d6a3e1c91c97a68a93a09ab075527ece29
Fil Maj <filip.maj@nitobi.com>
2009-12-22 14:03:26 -0800

    Updated Android PhoneGap source to latest from bowserj.

--- android/framework/src/com/phonegap/GeoBroker.java
+++ android/framework/src/com/phonegap/GeoBroker.java
@@ -3,6 +3,7 @@ package com.phonegap;
 import java.util.HashMap;
 
 import android.content.Context;
+import android.location.Location;
 import android.webkit.WebView;
 
 /*
@@ -23,8 +24,13 @@ public class GeoBroker {
 	}
 	
 	public void getCurrentLocation()
-	{
+	{				
 		GeoListener listener = new GeoListener("global", mCtx, 10000, mAppView);
+		Location loc = listener.getCurrentLocation();
+		String params = loc.getLatitude() + "," + loc.getLongitude() + ", " + loc.getAltitude() + "," + loc.getAccuracy() + "," + loc.getBearing();
+		params += "," + loc.getSpeed() + "," + loc.getTime();
+		mAppView.loadUrl("javascript:navigator.geolocation.gotCurrentPosition(" + params + ")");
+		listener.stop();
 	}
 	
 	public String start(int freq, String key)
