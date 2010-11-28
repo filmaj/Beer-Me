9542b993ee022b8a701d8f3b96dbc1c41e7fba79
Fil Maj <filip.maj@nitobi.com>
2010-11-28 02:49:26 -0800

    - Hooked up zoom controls, refactored zoom event handler.\n- Better positioning of beer (takes into account beer marker dimensions).\n- Handles orentiation changes gracefully. Hides zoom controls in landscape mode.

--- www/scripts/beerme.js
+++ www/scripts/beerme.js
@@ -11,21 +11,29 @@ function Zoom(app) {
      this.controlPosition = 155; //default 'top' style property for the control
      this.control = x$('#control');
      this.control.setStyle('top',this.controlPosition.toString() + 'px');
+     this.gauge = x$('#gauge');
 };
 Zoom.prototype = {
     into:function() {
-        if (this.level == this.max) return false;
-        this.level++;
-        this.controlPosition -= this.controlStep;
-        this.control.setStyle('top',this.controlPosition.toString() + 'px');
-        this.beer.updateLocation();
+        return this.zoom(true);
     },
     out:function() {
-        if (this.level == this.min) return false;
-        this.level--;
-        this.controlPosition += this.controlStep;
-        this.control.setStyle('top',this.controlPosition.toString() + 'px');
-        this.beer.updateLocation();
+        return this.zoom(false);
+    },
+    zoom:function(into) {
+        var self = this;
+        return function(evt) {
+            if (self.level == (into?self.max:self.min)) {
+                console.log('Zoom level is ' + self.level + ', no zoom allowed.');
+                return false;
+            }
+            self.level = (into?self.level+1:self.level-1);
+            console.log('Zoom level is now ' + self.level);
+            self.controlPosition = (into?(self.controlPosition - self.controlStep):(self.controlPosition + self.controlStep));
+            console.log('Control position is at ' + self.controlPosition);
+            self.control.setStyle('top',self.controlPosition.toString() + 'px');
+            self.beer.updateLocation();
+        }
     }
 }
 function BeerMe() {
@@ -34,6 +42,11 @@ function BeerMe() {
 	this.detail = x$('#detailScreen');
 	this.loading = x$('#backdrop');
 	this.map = x$('#body');
+	this.me = {
+	    element:x$('#me'),
+	    width:58,
+	    height:50
+	};
 	this.marker = {
 		width:45,
 		height:60
@@ -43,9 +56,8 @@ function BeerMe() {
 	 * Initializes controls (attaches events, positions DOM nodes) and then starts a location update.
 	 */	
 	var self = this;
-    // TODO: hook in zoom buttons.
-	x$('#plus').click(this.zoom.into);
-	x$('#minus').click(this.zoom.out);
+	x$('#plus').click(this.zoom.into());
+	x$('#minus').click(this.zoom.out());
 	x$('#closeBtn').click(function() {
 		self.detail.setStyle('display','none');
 	});
@@ -55,15 +67,23 @@ function BeerMe() {
 	x$('#about').click(function() {
 		self.showAbout();
 	});
+	// Orientation event detection and attaching for updating location and getting fresh map.
+	// Thank you StackOverflow: http://stackoverflow.com/questions/1649086/detect-rotation-of-android-phone-in-the-browser-with-javascript
+	var supportsOrientationChange = "onorientationchange" in window,
+        orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+    x$(window).on(orientationEvent, function() {
+        self.updateLocation();
+    });
 	this.updateLocation();
 	
 };
 BeerMe.prototype = {
+    // Cleanup function.
     clear:function() {
-        for (var i = 0; i < this.beerMarkers.length; i++) {
-            this.beerMarkers[i].node.remove();
+        while (this.beerMarkers.length) {
+            var beer = this.beerMarkers.pop().node[0];
+            beer.parentNode.removeChild(beer);
         }
-        this.beerMarkers = [];
         this.detail.setStyle('display','none');
     },
     /**
@@ -71,7 +91,6 @@ BeerMe.prototype = {
      * @param {Object} results An array of results containing place information.
      */
     // TODO: refactor this and the yql widget rendering into one function.
-    // TODO: also brittle with respect to working on various resolutions. Made for 320x480 right now.
     parseBeers:function(results) {
         // Extracts a value out of an XML document.
         var extractValue = function(node, childName) {
@@ -99,20 +118,30 @@ BeerMe.prototype = {
         },
         total = results.length;
         for (var i = 0; i < total; i++) {
-            // First grab lat/lng and see whether the marker will display on screen. If not, skip it.
+            // First grab lat/lng and convert to pixel coords.
             var lat = extractValue(results[i], 'lat');
             var lng = extractValue(results[i], 'lng');
             var rel = LLToXY(lng, lat, this.myCoords.longitude, this.myCoords.latitude, this.zoom.level);
-            var objX = 160 + rel.x;
-            var objY = 240 + rel.y;
-            // This sucks cuz it won't work on different screens. help?
-            if (objX < 1 || objX > 320-this.marker.width || objY < 1 || objY > 480-this.marker.height) continue;
+            var objX = (screen.width/2) + rel.x;
+            var objY = (screen.height/2) + rel.y;
+            
+            // Check whether the beer is off-screen.
+            if (objX < 1 || objX > (screen.width-(this.marker.width/2)) || objY < 1 || objY > (screen.height-this.marker.height)) {
+                console.log('Skipping a beer, off screen ('+objX+','+objY+')');
+                continue;
+            }
+            
+            // Adjust marker coords by proper marker offsets (half width, full height)
+            objX -= this.marker.width/2;
+            objY -= this.marker.height;
+            
             // Extract data about the place from XML.
             var link = extractValue(results[i], 'reviewlink');
             var title = extractValue(results[i], 'name');
             var address = extractValue(results[i], 'street') + ", " + extractValue(results[i], 'city') + ", " + extractValue(results[i], 'state') + ", " + extractValue(results[i], 'country');
             var phone = extractValue(results[i], 'phone');
             var rating = extractValue(results[i], 'overall');
+            
             // Create marker and inject into document.
             var img = x$('<img/>');
             img.addClass('beer');
@@ -124,12 +153,13 @@ BeerMe.prototype = {
             }
             img.setStyle('left', objX.toString() + 'px');
             img.setStyle('top', objY.toString() + 'px');
+            console.log('Rendering beer at ('+objX+','+objY+')');
             img.on('click', makeOnClick(info));
             this.beerMarkers.push({
                 'info': info,
                 'node': img
             });
-            this.map.bottom(img);
+            this.map.top(img);
         }
     },
     /**
@@ -140,12 +170,19 @@ BeerMe.prototype = {
         this.clear(); // remove old markers
         var dis = this;
         var win = function(position) {
-            console.log('Retrieved GPS coords.');
+            console.log('Retrieved GPS coords ('+position.coords.latitude+','+position.coords.longitude+')');
+            
             // Store coords.
             dis.myCoords = position.coords;
+            
             // Call for static google maps data.
-            var url = "http://maps.google.com/maps/api/staticmap?center=" + dis.myCoords.latitude + "," + dis.myCoords.longitude + "&zoom="+dis.zoom.level+"&size=320x480&maptype=roadmap&key=ABQIAAAASWkdhwcFZHCle_XL8gNI0hQQPTIxowtQGbc0PVHZZ3XLXr5GBhRKV3t_-63J9ZAJ2bYu3zsQdR9N-A&sensor=true"
+            var url = "http://maps.google.com/maps/api/staticmap?center=" + dis.myCoords.latitude + "," + dis.myCoords.longitude + "&zoom="+dis.zoom.level+"&size="+screen.width+"x"+screen.height+"&maptype=roadmap&key=ABQIAAAASWkdhwcFZHCle_XL8gNI0hQQPTIxowtQGbc0PVHZZ3XLXr5GBhRKV3t_-63J9ZAJ2bYu3zsQdR9N-A&sensor=true"
             x$('#map').attr('src',url);
+            
+            // If orientation is landscape, hide the zoom bar.
+            if (screen.width > screen.height) dis.zoom.gauge.setStyle('display','none');
+            else dis.zoom.gauge.setStyle('display','block');
+            
             // Call for beer data.
             dis.beerUpdate(dis.myCoords.latitude,dis.myCoords.longitude);
         };
@@ -162,19 +199,14 @@ BeerMe.prototype = {
     },
     getBeerFromBeerMapping:function(lat,lng) {
         var dis = this;
-		console.log('here');
         var url = "http://beermapping.com/webservice/locgeo/33aac0960ce1fd70bd6e07191af96bd5/" + lat + "," + lng + "," + this.getCurrentRadius();
         x$('#results').xhr(url, {
             async:true,
             callback:function(incoming) {
-                alert("incoming");
                 var xml = incoming?incoming.responseXML:this.responseXML;
-				alert(xml);
                 if (xml) {
-					console.log("test")
                     if (xml.childNodes[0]) {
                         var results =  xml.childNodes[0].childNodes;
-                        //alert('beermapping results: ' + results.length);
                         dis.parseBeers(results);
                     }
                 }
@@ -192,6 +224,7 @@ BeerMe.prototype = {
     },
     beerUpdate:function(lat,lng) {
         this.beerMarkers = [];
+        // TODO: enable YQL parsing.
         //this.getBeerFromYQL(lat,lng);
         this.getBeerFromBeerMapping(lat,lng);
     },
