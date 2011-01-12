81e65c85eda8557dc872e3de146c0178a2254e43
Fil Maj <filip.maj@nitobi.com>
2011-01-12 00:04:55 -0800

    Added basic loading screen for now and simple pin drops on map.

--- www/scripts/beerme.js
+++ www/scripts/beerme.js
@@ -5,9 +5,18 @@ function BeerMe(g) {
     this.position = {};
     this.radius = 20;
     this.map = null;
+    this.markers = [];
+    
+    // elements
+    this.blackout = x$('#blackout');
+    this.loading = {
+        el:x$('#loading'),
+        msg:x$('#loading h1'),
+        p:x$('#loading p')
+    }
     
     /* data adapters.
-    *  each object in the data array must have a `get` function that will parse a data source
+    *  each member in the data object must have a `get` function that will parse a data source
     *  and call `self.addMarker(obj)` to add markers for beer. marker template:
         {
             name:'',
@@ -17,7 +26,7 @@ function BeerMe(g) {
             phone:''
         }
     */
-    this.data = [
+    this.data = {
         'beermapping':{
             get:function(lat, lng, radius) {
                 var url = "http://localhost:8088/webservice/locgeo/33aac0960ce1fd70bd6e07191af96bd5/" + lat + "," + lng + "," + radius;
@@ -38,7 +47,7 @@ function BeerMe(g) {
                                     lng:get(loc, 'lng'),
                                     address:get(loc, 'street') + ', ' + get(loc, 'city') + ', ' + get(loc, 'state') + ', ' + get(loc, 'zip') + ', ' + get(loc, 'country'),
                                     phone:get(loc, 'phone')
-                                })
+                                });
                             })
                         } else {
                             navigation.notification.alert('Error','Cannot retrieve data from beermapping.com.');
@@ -47,7 +56,7 @@ function BeerMe(g) {
                 });
             }
         }
-    ];
+    };
     
     // refresh location and render map.
     this.refresh(function() {
@@ -58,19 +67,19 @@ function BeerMe(g) {
         });
         
         // draw map
-        var p = new g.maps.LatLng(this.position.coords.latitude, this.position.coords.longitude),
-            myOptions = {
+        this.LatLng = new g.maps.LatLng(this.position.coords.latitude, this.position.coords.longitude);
+        var myOptions = {
             zoom: 10,
-            center: p,
+            center: this.LatLng,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
             mapTypeControl:false,
             streetViewControl:false
         };
         this.map = new g.maps.Map(x$('#map')[0], myOptions);
         for (var d in this.data) {
-
             this.data[d].get(this.position.coords.latitude, this.position.coords.longitude, this.radius)
         }
+        this.hideLoading();
     });
 }
 BeerMe.prototype = {
@@ -88,7 +97,25 @@ BeerMe.prototype = {
         navigator.geolocation.getCurrentPosition(win, fail, options);
     },
     addMarker:function(item) {
+        var itemP = new google.maps.LatLng(item.lat, item.lng),
+            b = this.map.getBounds();
+        
         // first check if it is worth showing item in the current window.
-        console.log(item);
+        if (b.contains(itemP)) {
+            var marker = new google.maps.Marker({
+                map:this.map,
+                draggable:false,
+                animation: google.maps.Animation.DROP,
+                position:itemP
+            });
+            this.markers.push(marker);
+        }
+    },
+    showLoading:function() {
+        
+    },
+    hideLoading:function() {
+        this.blackout.setStyle('display','none');
+        this.loading.el.setStyle('display','none');
     }
 }
